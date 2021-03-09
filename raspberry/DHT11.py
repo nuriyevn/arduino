737de472dbcacb737890c071a692d187bc4761bb
nuriyevn <nuriyevn@gmail.com>
2021-03-09 18:53:01 +0200

    Update DHT11.py

--- raspberry/DHT11.py
+++ raspberry/DHT11.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
-
-while True:
-
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-    print (temperature)
-    print (humidity)
-    
-
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+if humidity is not None and temperature is not None:
+   print ('Temp=%.0f*C  Humidity=%.0f%%' % (temperature, humidity))
+else:
+   print ('Failed to get reading. Try again!')
